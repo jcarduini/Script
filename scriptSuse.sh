03b710bd91d101f2cef1d079e9ae6150873b20b0
julia <jcarduini@gmail.com>
2016-04-06 17:24:23 -0300

    colocar instruções cmake do cling

--- scriptSuse.sh
+++ scriptSuse.sh
@@ -13,6 +13,7 @@ MAIN_INSTALL_DIR=$MAIN_DIR/Binaries
 #-------------------------------------------------------------------------------------------
 # Versoes dos programas OK
 ZLIB_VERSION=zlib-1.2.8
+VALGRIND_VERSION=valgrind-3.11.0
 LLVM_VERSION=llvm-3.6.0.src
 CLANG_VERSION=clang
 CLING_VERSION=cling
@@ -20,6 +21,7 @@ CLING_VERSION=cling
 #-------------------------------------------------------------------------------------------
 # Caminhos para download dos programas OK
 PATH_TO_DOWNLOAD_ZLIB=zlib.net/zlib-1.2.8.tar.gz
+PATH_TO_DOWNLOAD_VALGRIND=valgrind.org/downloads/valgrind-3.11.0.tar.bz2
 PATH_TO_DOWNLOAD_LLVM=llvm.org/releases/3.6.0/llvm-3.6.0.src.tar.xz
 PATH_TO_DOWNLOAD_CLANG=llvm.org/releases/3.6.0/cfe-3.6.0.src.tar.xz
 PATH_TO_DOWNLOAD_CLING=https://root.cern.ch/download/root_v5.99.05.source.tar.gz
@@ -27,6 +29,7 @@ PATH_TO_DOWNLOAD_CLING=https://root.cern.ch/download/root_v5.99.05.source.tar.gz
 #-------------------------------------------------------------------------------------------
 # Caminhos para os programas baixados OK
 ZLIB_FILE=$MAIN_DOWNLOAD_DIR/$ZLIB_VERSION".tar.gz"
+VALGRIND_FILE=$MAIN_DOWNLOAD_DIR/$VALGRIND_VERSION".tar.bz2"
 LLVM_FILE=$MAIN_DOWNLOAD_DIR/$LLVM_VERSION".tar.xz"
 CLANG_FILE=$MAIN_DOWNLOAD_DIR/"cfe-3.6.0.src.tar.xz"
 CLING_FILE=$MAIN_DOWNLOAD_DIR/"root_v5.99.05.source.tar.gz"
@@ -34,6 +37,7 @@ CLING_FILE=$MAIN_DOWNLOAD_DIR/"root_v5.99.05.source.tar.gz"
 #-------------------------------------------------------------------------------------------
 # Caminhos para instalação dos programas  OK
 ZLIB_INSTALL_DIR=$MAIN_INSTALL_DIR/$ZLIB_VERSION
+VALGRIND_INSTALL_DIR=$MAIN_INSTALL_DIR/$VALGRIND_VERSION
 LLVM_INSTALL_DIR=$MAIN_INSTALL_DIR/$LLVM_VERSION
 CLANG_INSTALL_DIR=$LLVM_INSTALL_DIR/tools/$CLANG_VERSION
 CLING_INSTALL_DIR=$MAIN_INSTALL_DIR/$CLING_VERSION
@@ -41,8 +45,8 @@ CLING_INSTALL_DIR=$MAIN_INSTALL_DIR/$CLING_VERSION
 #-------------------------------------------------------------------------------------------
 # Instalacao dos compiladores gcc c++ e fortran, cmake OK
 sudo zypper install cmake gcc gcc-fortran gcc-c++ git bash make binutils \
-xorg-x11-libX11-devel xorg-x11-libXpm-devel xorg-x11-devel \
-xorg-x11-proto-devel xorg-x11-libXext-devel
+ocaml ocaml-native-compilers ocaml-findlib oasis kate\
+xorg-x11-libX11-devel xorg-x11-libXpm-devel xorg-x11-devel xorg-x11-proto-devel xorg-x11-libXext-devel
 
 #-------------------------------------------------------------------------------------------
 # Criacao dos diretorios padroes OK
@@ -60,6 +64,31 @@ fi
 if ! [ -e $MAIN_INSTALL_DIR ];
 	then mkdir $MAIN_INSTALL_DIR;
 fi
+
+#-------------------------------------------------------------------------------------------
+	# Pre-requisitos do Cling
+#-------------------------------------------------------------------------------------------
+# Valgrind
+	# Criacao do diretorio de instalacao
+if ! [ -e $VALGRIND_INSTALL_DIR ];
+	then mkdir $VALGRIND_INSTALL_DIR;
+fi
+	
+	# Download 
+if ! [ -e $VALGRIND_FILE ];
+	then wget -P $MAIN_DOWNLOAD_DIR $PATH_TO_DOWNLOAD_VALGRIND;
+fi
+
+	# Descompatacao do source
+cd $MAIN_DOWNLOAD_DIR
+tar -jxvf $VALGRIND_FILE
+
+	# Configuracao e instalacao 
+cd $VALGRIND_VERSION
+CC=gcc CXX=g++ FC=gfortran F77=gfortran ./configure --prefix=$VALGRIND_INSTALL_DIR
+make
+make install
+
 
 #-------------------------------------------------------------------------------------------
 	# Pre-requisitos do Clang
@@ -84,7 +113,6 @@ cd $ZLIB_VERSION			# Sources/zlib
 CC=gcc CXX=g++ FC=gfortran F77=gfortran ./configure --prefix=$ZLIB_INSTALL_DIR
 make all -j$N
 make install
-
 #-------------------------------------------------------------------------------------------
 # Clang OK
 	# Download
@@ -143,11 +171,14 @@ mv root $LLVM_VERSION/tools/$CLING_VERSION
 	# Configuracao LLVM
 cd $LLVM_INSTALL_DIR			# Binaries/LLVM
 CC=gcc CXX=g++ FC=gfortran F77=gfortran 
-#-G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$LLVM_INSTALL_DIR -DPYTHON_EXECUTABLE=$python-DLLVM 
-cmake $MAIN_DOWNLOAD_DIR/$LLVM_VERSION
-cmake --build .
-cmake --build . --target install
+cmake -G "Unix Makefiles" $MAIN_DOWNLOAD_DIR/$LLVM_VERSION
+#cmake --build .
+#cmake --build . --target install
 
+	# Instalacao LLVM
+cd $LLVM_INSTALL_DIR
+make 
+make install
 
 #-------------------------------------------------------------------------------------------
 # Finalizacao - exportar links para os enderecos padroes

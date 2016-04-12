e5b5fa89cc9f34cc6242e686aacf4ec6a10b4889
julia <jcarduini@gmail.com>
2016-04-12 18:22:16 -0300

    aparentemente esta versao contem um bug na instalacao

--- scriptClone.sh
+++ scriptClone.sh
@@ -1,19 +1,34 @@
+#!/bin/bash
+#--------------------------------------------------------------------------------------------
 # Diretorio base de downloads e instalacoes
 MAIN_DIR=$HOME/Softwares
 
+#--------------------------------------------------------------------------------------------
 # Diretorio de downloads
 MAIN_DOWNLOAD_DIR=$MAIN_DIR/Sources
 
+#--------------------------------------------------------------------------------------------
 # Diretorio de instalacoes
 MAIN_INSTALL_DIR=$MAIN_DIR/Binaries
 
+#--------------------------------------------------------------------------------------------
+# Versoes
+ALL_VERSION=root_v6.02.02.source
+
+#--------------------------------------------------------------------------------------------
 # Caminho para baixar
-PATH_TO_DOWNLOAD_ALL=https://raw.githubusercontent.com/karies/cling-all-in-one/master/clone.sh
+PATH_TO_DOWNLOAD_ALL=https://root.cern.ch/download/root_v6.02.02.source.tar.gz
+#PATH_TO_DOWNLOAD_ALL=https://raw.githubusercontent.com/karies/cling-all-in-one/master/clone.sh
 
+#--------------------------------------------------------------------------------------------
 # Script que sera executado contendo LLVM, CLANG e CLING
-ALL_FILE=$MAIN_DOWNLOAD_DIR/"clone.sh"
+ALL_FILE=$MAIN_DOWNLOAD_DIR/$ALL_VERSION".tar.gz"
 
+#--------------------------------------------------------------------------------------------
+# Diretorios de instalacao
+ALL_INSTALL_DIR=$MAIN_INSTALL_DIR/$ALL_VERSION
 
+#--------------------------------------------------------------------------------------------
 # Criacao dos diretorios padroes
 	# Principal
 if ! [ -e $MAIN_DIR ];
@@ -30,9 +45,13 @@ if ! [ -e $MAIN_INSTALL_DIR ];
 	then mkdir $MAIN_INSTALL_DIR;
 fi
 
+#--------------------------------------------------------------------------------------------
 # Instalacao do CMAKE, pre-requisito para compilar cling
-sudo zypper install cmake
+sudo zypper install git bash make gcc-c++ gcc binutils \
+xorg-x11-libX11-devel xorg-x11-libXpm-devel xorg-x11-devel \
+xorg-x11-proto-devel xorg-x11-libXext-devel
 
+#--------------------------------------------------------------------------------------------
 # LLVM + CLANG + CLING
 	# Download arquivo
 #cd $MAIN_DONLOAD_DIR #Softwares/Sources
@@ -40,6 +59,12 @@ if ! [ -e $ALL_FILE ];
 	then wget -P $MAIN_DOWNLOAD_DIR $PATH_TO_DOWNLOAD_ALL;
 fi
 
-	# Configuracao do script 
+# Descompactacao do source
 cd $MAIN_DOWNLOAD_DIR
-sh "clone.sh"
+tar -xzvf $ALL_FILE
+
+	# Configuracao  
+cd $MAIN_DOWNLOAD_DIR/$ALL_VERSION # Sources/root
+./configure --prefix=/usr/local 
+make -j2
+make install
